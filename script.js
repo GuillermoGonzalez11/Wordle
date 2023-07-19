7e7b6860cd7f86ef4fcb5db034e8506c0438dd5c
GuillermoGonzalez11 <139256299+GuillermoGonzalez11@users.noreply.github.com>
2023-07-19 16:35:31 -0400

    proyect.actualizado

--- script.js
+++ script.js
@@ -1,69 +1,33 @@
-let lista = ["ARBOL","ANGEL","MARCO", "CARRO","BARRO","PERRO"]
-//let palabra = "ARBOL"
-//let palabra = lista[Math.floor(Math.random()*lista.length)];
+const apiKey ="20d66fe92d055ea9c827e1c8dbe9f05d";
+const ciudad ="Fram,py"
+const apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&APPID=${apiKey}&units=metric`
+let dato;
+const VIENTO = document.getElementById("viento")
+const ESTADO = document.getElementById("estado")
+const TEMPERATURA = document.getElementById("temperatura")
 
-let intentos = 6;
-const API = "https://random-word-api.herokuapp.com/word?length=5&lang=es"
-fetch (API)
-.then(response => response.json ())
-.then(response => {
-    console.log (response)
-    palabra = response[0].toUpperCase()
-    console.log(palabra)
+
+fetch(apiUrl)
+.then(response=> response.json())
+.then(response=>{
+    console.log(response)
+    dato = response
+    let temperatura = dato.main.temp //"Rain"
+    let tempMin = dato.main.temp_min
+    let tempMax = dato.main.temp_max
+    TEMPERATURA.innerHTML = temperatura+"°"
+    console.log('temp min', tempMin)
+    console.log('temp max', tempMax)
+    let viento = dato.wind.speed
+    VIENTO.innerHTML=viento+"km/h"
+    console.log(viento)
+    let clima = dato.weather[0].main
+    ESTADO.innerHTML = estado+
+    console.log(clima)
+
 })
-.catch(err => palabra = lista [Math.floor(Math.random()*lista.length)]);
+.catch(
+    err => console.error(err)
+)
 
-
-const BOTON = document.getElementById("guess-button");
-
-BOTON.addEventListener("click", intentar);
-
-function intentar(){
-
-    const GRID = document.getElementById('grid');
-    const ROW = document.createElement("div");
-    ROW.className = "row";
-    const INTENTO = leerIntento();
-    if (INTENTO === palabra ) {
-        terminar
-        ("<h1> GANASTE! 👏🤯🥳</h1>")
-    
-    }
-    for (let i in palabra){
-        const SPAN = document.createElement("span")
-        SPAN.className = "letter";
-        if (INTENTO[i]===palabra[i]){
-        SPAN.innerHTML = INTENTO[i]
-           SPAN.style.backgroundColor = "green";
-        } else if( palabra.includes(INTENTO[i]) ) {
-            SPAN.innerHTML = INTENTO[i]
-            SPAN.style.backgroundColor = "yellow";
-        } else {
-            SPAN.innerHTML = INTENTO[i]
-            SPAN.style.backgroundColor = "gray";
-        }
-        ROW.appendChild(SPAN)
-    }
-    GRID.appendChild(ROW)
-    intentos--
-	if (intentos==0){
-        terminar("<h1>PERDISTE!😭😥😓</h1>")
-        
-    }
-
-}
-
-
-function leerIntento(){
-    let intento = document.getElementById("guess-input");
-    intento = intento.value;
-    intento = intento.toUpperCase(); 
-    return intento;
-}
-function terminar(mensaje){
-    const INPUT = document.getElementById("guess-input");
-    INPUT.disabled = true;
-    BOTON.disabled = true;
-    let contenedor = document.getElementById('guesses');
-    contenedor.innerHTML = mensaje;
-}
+console.log(dato)
